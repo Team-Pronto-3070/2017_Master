69c3b98b8c5ecbbde386abb0117adcc5fea33054
StevenSuarez <ss0980060@k12.shorelineschools.org>
2017-02-07 19:13:57 -0800

    added Drive class

---
+++ prod/src/org/usfirst/frc/team3070/robot/Drive.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team3070.robot;
+// TODO: install and import can talon classimport com
+public class Drive {
+	
+private  CANTalon talFR;
+ private CANTalon talFL;
+private	CANTalon talBR;
+private CANTalon talBL;
+	
+	public Drive(int tal1, int tal2, int tal3, int tal4)
+	{
+		// Initialize talon object
+	}
+	
+	public void drive(double leftSpeed, double rightSpeed)
+	{
+		talFR.set(rightSpeed);
+		talBR.set(rightSpeed);
+		talFL.set(leftSpeed);
+		talBL.set(leftSpeed);
+	}
+}
