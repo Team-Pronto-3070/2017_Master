8c6cca5d68e5f48af22d3f58c0de67edc96a80dc
AwesomeMP8 <Ihascrudmail@gmail.com>
2017-02-10 17:06:59 -0800

    Fixed some climber code

--- FinalRobot/src/org/usfirst/frc/team3070/robot/Climb.java
+++ FinalRobot/src/org/usfirst/frc/team3070/robot/Climb.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team3070.robot;
 
 public class Climb extends Robot implements Pronstants {
-	public void thingDoer() {
-		if (joystick.getRawButton(1)) {
+	public void checkClimbInput(boolean button1, boolean button2) {
+		if (button1) {
 			talC1.set(AUTO_CLIMB_SPEED);
 			talC2.set(AUTO_CLIMB_SPEED);
 		}
@@ -10,7 +10,8 @@ public class Climb extends Robot implements Pronstants {
 			talC1.set(0);
 			talC2.set(0);
 		}
-		if (joystick.getRawButton(2)) {
+		//TODO should this button1 and button2?
+		if (button2) {
 			talC1.set(-AUTO_CLIMB_SPEED);
 			talC2.set(-AUTO_CLIMB_SPEED);
 		}
