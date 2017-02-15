bbd935ea4fc20ed9e48f54509dd1f1fa764785e4
Alex Holguin <Alex Holguin>
2017-02-14 16:39:58 -0800

    added vision string manipulation

--- FinalRobot/src/org/usfirst/frc/team3070/robot/Sensors.java
+++ FinalRobot/src/org/usfirst/frc/team3070/robot/Sensors.java
@@ -6,5 +6,12 @@ public class Sensors {
 	public static double calculateHeading(double sh) {
 		return imu.getHeading()-sh;
 	}
+	public double[] findCamOut(String camIn) {
+		camIn = camIn.substring(14);
+		String[] camSplit = camIn.split(",");
+		String[] strOut = {camSplit[0],camSplit[1].substring(1, camSplit[1].length()-1),camSplit[2].substring(6)};
+		double[] out = {Double.parseDouble(strOut[0]), Double.parseDouble(strOut[1]), Double.parseDouble(strOut[2])};
+		return out;
+	}
 
 }
