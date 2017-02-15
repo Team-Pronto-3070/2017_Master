33860341546c6ee0a8fb6c26d9ac242ccaf45c24
AwesomeMP8 <Ihascrudmail@gmail.com>
2017-02-14 16:58:31 -0800

    climber comments

--- FinalRobot/src/org/usfirst/frc/team3070/robot/Climb.java
+++ FinalRobot/src/org/usfirst/frc/team3070/robot/Climb.java
@@ -3,7 +3,9 @@ package org.usfirst.frc.team3070.robot;
 import com.ctre.CANTalon;
 
 public class Climb {
+	//Defines talons for the climber
 	static CANTalon talC1, talC2;
+	//climber constructor
 	public Climb()
 	{
 		//defines the talon variables
@@ -17,18 +19,27 @@ public class Climb {
 		talC2.setCurrentLimit(Pronstants.CLIMB_CURRENT_LIMIT);
 	}
 	public void checkClimbInput(boolean button1, boolean button2) {
+		//maps climber to joystick buttons
+		//checks if button1 is pressed and button2 is not
 		if (button1 && !button2) {
+			//If true, climb up
 			talC1.set(Pronstants.AUTO_CLIMB_SPEED);
 			talC2.set(Pronstants.AUTO_CLIMB_SPEED);
 		}
+		//checks if button2 is pressed and button1 is not
 		if (button2 && !button1) {
+			//If true, climb down
 			talC1.set(-Pronstants.AUTO_CLIMB_SPEED);
 			talC2.set(-Pronstants.AUTO_CLIMB_SPEED);
 		}
+		//checks if button1 and button2 are pressed
 		if (button1 && button2) {
+			//If true, set climber to 0
+			//This prevents the climber from trying to go in 2 directions at once
 			talC1.set(0);
 			talC2.set(0);
 		}
+		//In any other case, set climber to 0
 		else {
 			talC1.set(0);
 			talC2.set(0);
